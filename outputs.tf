0097ab63489a16d386143c30070f2b0a62ab61aa
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2025-02-07 18:11:05 -0300

    adjust output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "rds_endpoint_order" {
-  value       = aws_db_instance.rds_order.endpoint
+  value       = "jdbc:postgresql://${aws_db_instance.rds_order.endpoint}/${aws_db_instance.rds_order.db_name}"
   description = "Ip para fazer conexão com o banco de order service"
 }
 
 output "rds_endpoint_person" {
-  value       = aws_db_instance.rds_person.endpoint
+  value       = "jdbc:postgresql://${aws_db_instance.rds_person.endpoint}/${aws_db_instance.rds_person.db_name}"
   description = "Ip para fazer conexão com o banco de person service"
 }
